27426380cfa6332c564babd8ec59edb0c1a02a41
sansi.zhang <sansi.zhang@g42.ai>
2025-03-17 17:13:22 +0800

    简易词典

--- 简易词典/main.go
+++ 简易词典/main.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type Payload struct {
 	Source    string
 	TransType string `json:"trans_type"`
 	RequestId string `json:"request_id"`
-	detect    bool
+	Detect    bool
 }
 
 func main() {
@@ -18,9 +22,25 @@ func main() {
 		var url string = "http://api.interpreter.caiyunai.com/v1/translator"
 		var token string = "3975l6lr5pcbvidl6jl2"
 
+		requestMap := map[string]interface{}
+		requestMap["source"] = dirct
+		requestMap["trans_type"] = "auto2zh"
+		requestMap["request_id"] = "demo"
+		requestMap["detect"] = true
+
+		requestBody, err := json.Marshal(requestMap)
+
 		var headers map[string]interface{}
 		headers["content-type"] = "application/json"
 		headers["x-authorization"] = "token " + token
+
+		payload := Payload{
+			Source:    dirct,
+			TransType: "auto2zh",
+			RequestId: "demo",
+			Detect:    true,
+		}
+		http.Post(url)
 
 		fmt.Println(dirct)
 	}
